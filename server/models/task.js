87a6786e7d11fd675fd603fa32e30fb97c60a1f9
EvheniyZl <61193708+EvheniyZl@users.noreply.github.com>
2025-02-12 19:19:56 +0200

    upd

--- server/models/task.js
+++ server/models/task.js
@@ -30,6 +30,7 @@ const taskSchema = new Schema(
             "bug",
             "completed",
             "commented",
+            "subtask_added",
           ],
         },
         activity: String,
